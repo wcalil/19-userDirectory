9fc54a5c9bc7e9cbd45529cc801bbcd7be1d9be0
Wilson Calil <wilson.calil@outlook.com>
2020-05-10 17:08:09 -0500

    Completed the filter and sort react functions.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
 import React, {useState} from 'react';
 import EmployeeTable from './components/EmployeeTable'
 import './App.css';
+import employeeList from "./employeeList.json";
 
 function App() {
+ 
   const [ search, setSearch]= useState('')
-
-
-
-  const employeeList = [
-    {
-      name: 'bob',
-      job: 'developer',
-      pay: 100
-    },
-    {
-      name: 'john',
-      job: 'designer',
-      pay: 120
-    }
-  ]
+  const [ empList, setEmpList] = useState(employeeList)
+ 
+  const sortEmps = () => {
+    console.log('sorting')
+    console.log(empList)
+    const sortedList = [...empList].sort((a, b)=>{
+      if ( a.name > b.name) {
+        return 1
+      }
+      if ( a.name < b.name) {
+        return -1
+      }
+      return 0
+    })
+    setEmpList(sortedList)
+  }
   
   return (
-    <div className="App">
-      <input onChange={e=>setSearch(e.target.value)} value={search} />
-      <EmployeeTable employeeList={employeeList} search={search} />
+    <div>
+    <div className="row center">
+      <h1>Employee Directory</h1>
+    </div>
+    <div className="row center">
+      <div className="col-md-4"></div>
+    <div className="App col-md-4 center" >
+      <br></br>
+      <input 
+      className="input-group input-group-lg"
+      onChange={e=>setSearch(e.target.value)}
+      value={search}
+      placeholder="Search For An Employee"
+    />
+      <br></br><br></br>
+      <EmployeeTable
+      className="employeeTable"
+      sortEmps={sortEmps}
+      employeeList={empList}
+      search={search}
+    />
+    </div>
+    <div className="col-md-4"></div>
+    </div>
     </div>
   );
 }
--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 const EmployeeTable = props => {
     console.log(props.search)
+    console.log(props.employeeList)
+    function filter(employee){
+        return(
+            employee.name.includes(props.search)
+        )
+    }
+
+    
     const makeEmpRows = () => {
-        return props.employeeList.map(emp=>{
+        return props.employeeList.filter(filter).map(emp=>{
             return (
-                <tr>
+                <tr key={emp.name}>
                     <td>{emp.name}</td>
                     <td>{emp.job}</td>
                     <td>{emp.pay}</td>
@@ -14,13 +22,18 @@ const EmployeeTable = props => {
         })
     }
     return (
-        <table>
-            <thead>
-                <td>name</td>
+        <table className="table">
+            <thead className="tableHead">
+                <tr>
+                <td onClick={props.sortEmps}>name</td>
                 <td>position</td>
                 <td>pay</td>
+                </tr>
             </thead>
+            <tbody>
+
             { makeEmpRows() }
+            </tbody>
         </table>
     )
 }
